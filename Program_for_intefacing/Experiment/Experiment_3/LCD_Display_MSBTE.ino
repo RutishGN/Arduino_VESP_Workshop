7c4d8030e36103dcc8d39d20165ac25f5bcb809c
RutishGN <94203535+RutishGN@users.noreply.github.com>
2025-03-06 14:08:16 +0530

    Update LCD_Display_MSBTE.ino

--- Program_for_intefacing/Experiment/Experiment_3/LCD_Display_MSBTE.ino
+++ Program_for_intefacing/Experiment/Experiment_3/LCD_Display_MSBTE.ino
@@ -13,3 +13,26 @@ void setup() {
 
 void loop() {}
 
+
+//CODE 2 
+// C++ code
+//
+#include <Adafruit_LiquidCrystal.h>
+
+
+Adafruit_LiquidCrystal lcd_1(0);
+
+void setup()
+{
+  lcd_1.begin(16, 2);
+
+  lcd_1.print("MSBTE");
+}
+
+void loop()
+{
+  lcd_1.setCursor(0, 1);
+  lcd_1.setBacklight(1);
+  
+  
+}
